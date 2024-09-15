ee099e1e5eeaa8b40529fd986bafe99ab0c72321
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-09-15 19:00:17 +0900

    Fix: DataSourceConfiguration 제거
    h2를 임베드 시켜서 불필요

--- src/main/java/com/ticket/user/domain/User.java
+++ src/main/java/com/ticket/user/domain/User.java
@@ -1,16 +1,23 @@
 package com.ticket.user.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
-    private String id;
-    private String pw;
+    @Id
+    private String userId;
+    private String userPw;
+
+    public User() {}
 
     public User(String userId, String userPw) {
-        this.id = userId.trim();
-        this.pw = userPw.trim();
+        this.userId = userId.trim();
+        this.userPw = userPw.trim();
     }
 
     public boolean checkPassword(String password) {
-        return this.pw.equals(password);
+        return this.userPw.equals(password);
     }
 }
